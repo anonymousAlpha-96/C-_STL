83a409072720793f3118422dfef27a96d6c8c8b8
Ankit <43381040+anonymousAlpha-96@users.noreply.github.com>
2021-10-14 07:05:51 +0530

    Create STL_Stack.cpp

---
+++ STL_Stack.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+int main()
+{
+    stack<string> s;
+
+    s.push("A");
+    s.push("B");
+    s.push("C");
+    s.push("D");
+    cout<<"\nString top element : ";
+    cout<<s.top();
+    s.pop();
+    cout<<"\nString top element : ";
+    cout<<s.top();
+      s.pop();
+    cout<<"\nString top element : ";
+      cout<<s.top();
+      s.pop();
+    cout<<"\nString top element : ";
+      cout<<s.top();
+      s.pop();
+    cout<<"\nString top element : ";
+    cout<<s.top();
+//    for(int i:s)
+//    {
+//        cout<<" "<<i;
+//    }
+
+    return 0;
+}
